7d047e7a6eff0eeadfcc03f52adeeec93b909ca9
andrew <tkachandr62@gmail.com>
2024-03-15 18:35:32 +0100

    update nest to v10

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,18 @@
 import 'dotenv/config';
 
-import { defineConfig, Config } from 'drizzle-kit';
+import { Config } from 'drizzle-kit';
 
-const config = defineConfig({
+const config = {
   schema: './src/database/drizzle/schema.ts',
   out: './src/database/drizzle/migrations',
   driver: 'pg',
   dbCredentials: {
-    host: process.env.PG_HOST,
+    host: process.env.PG_HOST!,
     port: Number.parseInt(String(process.env.PG_PORT), 10),
     user: process.env.PG_USERNAME,
     password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    database: process.env.PG_DATABASE!,
   },
-} as Config);
+} satisfies Config;
 
 export default config;
